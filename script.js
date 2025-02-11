78f1287c5fa0e3b1fde2d98a67324fed3e444f8c
Taine Buchan <tainebuchanwork@gmail.com>
2025-02-11 17:08:54 +1300

    couple more spacing updates

---
+++ script.js
@@ -0,0 +1,32 @@
+let blogDate = document.getElementById('date')
+let body = document.querySelector('body')
+
+function generateDate() {
+  let monthArray = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ]
+
+  if (blogDate.textContent === '') {
+    const date = new Date()
+
+    let day = date.getDate()
+    let month = date.getMonth()
+    let year = date.getFullYear() % 100 // I only want the last two digits of the year so 2025 % 100 becomes 25.
+    let fullDate = `${day} ${monthArray[month]} ${year}`
+
+    blogDate.textContent = fullDate
+  }
+}
+
+generateDate()
